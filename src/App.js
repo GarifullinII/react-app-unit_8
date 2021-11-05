ef9202ecdf45c60756cd7fc6cb691a649d46e030
GarifullinII <ildaridrisovich@gmail.com>
2021-11-06 00:46:05 +0400

    add h2 React.createElement

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ function App() {
 
   const h1 = React.createElement('h1', {}, 'unit_08');
 
+  const h2 = React.createElement('h2', {
+    'className': 'text-orange' 
+  }, 'header 2')
+
   return (
     <>
       {h1}
+      {h2}
     </>
   );
 }
